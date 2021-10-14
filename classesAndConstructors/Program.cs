bfe3abdeb842fdcaf073f8fb13c125a48461135c
AnnKemp <ann.kemp@scarlet.be>
2021-10-14 18:58:47 +0200

    with constructor

--- classesAndConstructors/Program.cs
+++ classesAndConstructors/Program.cs
@@ -2,11 +2,22 @@
 
 namespace classesAndConstructors
 {
-    class Program
+    // Create a Car class
+    class Car
     {
+        public string model;  // Create a field
+
+        // Create a class constructor for the Car class
+        // by repeating the class-name with brackets behind it and public in front of it
+        public Car()
+        {
+            model = "Mustang"; // Set the initial value for model
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+            Console.WriteLine(Ford.model);  // Print the value of model
         }
     }
 }
